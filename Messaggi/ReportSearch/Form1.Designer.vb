ab92699f735955ddffa913d81f5bef01cf3c79c5
raffysommy <raffysommy@gmail.com>
2010-06-29 18:13:14 +0200

    first commit
    Signed-off-by: raffysommy <raffysommy@gmail.com>

---
+++ Messaggi/ReportSearch/Form1.Designer.vb
@@ -0,0 +1,117 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form esegue l'override del metodo Dispose per pulire l'elenco dei componenti.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Richiesto da Progettazione Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: la procedura che segue è richiesta da Progettazione Windows Form
+    'Può essere modificata in Progettazione Windows Form.  
+    'Non modificarla nell'editor del codice.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.TextBox1 = New System.Windows.Forms.TextBox
+        Me.TextBox2 = New System.Windows.Forms.TextBox
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.TextBox3 = New System.Windows.Forms.TextBox
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(196, 66)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(182, 73)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Inizio (Effettua Prima il login nella schermata sottostante)"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Location = New System.Drawing.Point(-3, 173)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.Size = New System.Drawing.Size(591, 449)
+        Me.WebBrowser1.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(8, 20)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(130, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "inizio nachrichten.php?id="
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(126, 16)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(84, 20)
+        Me.TextBox1.TabIndex = 4
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(393, 20)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(84, 20)
+        Me.TextBox2.TabIndex = 6
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(272, 23)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(124, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "fine nachrichten.php?id="
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(468, 147)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.ReadOnly = True
+        Me.TextBox3.Size = New System.Drawing.Size(84, 20)
+        Me.TextBox3.TabIndex = 7
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(589, 618)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.WebBrowser1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents WebBrowser1 As System.Windows.Forms.WebBrowser
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+
+End Class
---
+++ Messaggi/ReportSearch/Form1.vb
@@ -0,0 +1,85 @@
+﻿Imports System.IO
+
+
+Public Class Form1
+   
+
+
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        Dim navto As String
+        Dim objStreamWriter As StreamWriter
+
+        'WebBrowser1.Navigate("http://s9.travian.it/nachrichten.php?id=0000000")
+        'System.Threading.Thread.Sleep(150)
+        Dim counterrepovuoto As Integer = 250  'WebBrowser1.Document.All.Count.ToString()
+
+        objStreamWriter = New System.IO.StreamWriter("C:\Report.txt")
+
+
+        Dim fine As Integer = TextBox2.Text
+
+        'Dim objStreamWriter As StreamWriter
+
+
+        'objStreamWriter = New System.IO.StreamWriter("C:\Report.txt")
+
+        Dim start As Integer = TextBox1.Text
+
+
+        Dim startstring As String = start
+
+        While start < fine
+            start = start + 1
+
+            navto = "http://s9.travian.it/nachrichten.php?id=" + startstring
+            startstring = start + 1
+            TextBox3.Text = start
+            WebBrowser1.Navigate(navto)
+
+            System.Threading.Thread.Sleep(700)
+            Dim counterrepo As Integer = WebBrowser1.Document.All.Count.ToString()
+
+
+            MsgBox(counterrepo)
+
+            navto = "http://s9.travian.it/nachrichten.php?id=" + startstring
+            Dim navstring As String = startstring - 2
+
+            If counterrepo > 250 Then
+
+
+
+
+                objStreamWriter.WriteLine(navto+navstring)
+
+            End If
+            
+            'objStreamWriter.WriteLine(navto)
+
+            'End If
+
+        End While
+
+
+        objStreamWriter.Close()
+        MsgBox("Troverai il file con i risultati in c:\Report.txt :)")
+
+
+
+
+    End Sub
+   
+  
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        WebBrowser1.Navigate("http://s9.travian.it")
+
+
+    End Sub
+
+
+    Public Sub WebBrowser1_DocumentCompleted(ByVal sender As System.Object, ByVal e As System.Windows.Forms.WebBrowserDocumentCompletedEventArgs) Handles WebBrowser1.DocumentCompleted
+        
+    End Sub
+End Class
